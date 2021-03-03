764069c55351f2e967199b924716b1788c817b7c
Gelfande, Tala <tala.gelfande@accenture.com>
2021-03-03 17:05:04 +0200

    render field using for loop

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,133 +1,138 @@
 import React from "react";
-import BlackFigure from "../figures/BlackFigure";
-import WhiteFigure from "../figures/WhiteFigure";
+// import BlackFigure from "../figures/BlackFigure";
+// import WhiteFigure from "../figures/WhiteFigure";
 
 import "./Field.scss";
 
+const squares = [];
+
+for (let i = 0; i < 64; i++) {
+  squares.push(<div className="square"></div>);
+}
+
 function Field() {
-  return (
-    <div className="field">
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
+  return <div className="field">{squares}</div>;
+  // <div className="field">
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
 
-      <div className="square" />
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <BlackFigure />
-      </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
 
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <BlackFigure />
-      </div>
-      <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <BlackFigure />
+  //   </div>
+  //   <div className="square" />
 
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
 
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
-      <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
+  //   <div className="square" />
 
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
 
-      <div className="square" />
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <WhiteFigure />
-      </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
 
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
-      <div className="square">
-        <WhiteFigure />
-      </div>
-      <div className="square" />
-    </div>
-  );
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
+  //   <div className="square">
+  //     <WhiteFigure />
+  //   </div>
+  //   <div className="square" />
+  // </div>
 }
 
 export default Field;
