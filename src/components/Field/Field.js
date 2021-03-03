ea0596c7fab38ab068bedad8da1bf6d2b38126e6
Gelfande, Tala <tala.gelfande@accenture.com>
2021-03-03 17:08:32 +0200

    create FieldCell component

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,13 +1,14 @@
 import React from "react";
 // import BlackFigure from "../figures/BlackFigure";
 // import WhiteFigure from "../figures/WhiteFigure";
+import FieldCell from "../FieldCell";
 
 import "./Field.scss";
 
 const squares = [];
 
 for (let i = 0; i < 64; i++) {
-  squares.push(<div className="square"></div>);
+  squares.push(<FieldCell />);
 }
 
 function Field() {
---
+++ src/components/FieldCell/FieldCell.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./FieldCell.scss";
+
+function FieldCell() {
+  return <div className="cell"></div>;
+}
+
+export default FieldCell;
