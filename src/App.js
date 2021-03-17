5c1e14ec8f473da6943e3c8e2666c8041fbdb9bf
Tal Gelfand <talj.gelfand@gmail.com>
2021-03-17 08:06:33 +0200

    remove fail screen from main page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from "react";
 import FirstButton from "./components/buttons/FirstButton/FirstButton";
 import SecondButton from "./components/buttons/SecondButton/SecondButton";
 import Field from "./components/Field";
-import FailScreen from "./components/screens/FailScreen";
 import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
-      {/* <Title />
+      <Title />
       <FirstButton />
       <Field />
-      <SecondButton /> */}
-      <FailScreen />
+      <SecondButton />
     </div>
   );
 }
