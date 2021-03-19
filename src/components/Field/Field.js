d3c7bc755efdf235a14c5e4050437ba414fa867d
Tal Gelfand <talj.gelfand@gmail.com>
2021-03-19 17:12:08 +0200

    render aray of objects (bug)

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -5,12 +5,18 @@ import "./Field.scss";
 
 const squares = [];
 
-for (let i = 0; i < 64; i++) {
-  squares.push(<FieldCell />);
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    squares[i][j] = <FieldCell />;
+  }
 }
 
 function Field() {
-  return <div className="field">{squares}</div>;
+  squares.map((square) => {
+    return square.map((cell) => {
+      return <div className="field">{cell}</div>;
+    });
+  });
 }
 
 export default Field;
