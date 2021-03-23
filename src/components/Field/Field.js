b6d3d7abfc51efd86f4a0b8925143f5f91c450c5
Tal Gelfand <talj.gelfand@gmail.com>
2021-03-23 22:44:08 +0200

    implement drag and drop api

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useReducer } from "react";
 import FieldCell from "../FieldCell";
 
 import "./Field.scss";
 
-const squares = [];
+const Field = () => {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_DROP_DEPTH":
+        return { ...state, dropDepth: action.dropDepth };
+      case "SET_IN_DROP_ZONE":
+        return { ...state, inDropZone: action.inDropZone };
+      case "ADD_FILE_TO_LIST":
+        return { ...state, fileList: state.fileList.concat(action.files) };
+      default:
+        return state;
+    }
+  };
 
-for (let i = 0; i < 8; i++) {
-  squares.push([]);
-  for (let j = 0; j < 8; j++) {
-    squares[i].push(<FieldCell key={i + "" + j} />);
+  const [data, dispatch] = useReducer(reducer, {
+    dropDepth: 0,
+    inDropZone: false,
+    fileList: [],
+  });
+
+  const squares = [];
+
+  for (let i = 0; i < 8; i++) {
+    squares.push([]);
+    for (let j = 0; j < 8; j++) {
+      squares[i].push(
+        <FieldCell key={i + "" + j} data={data} dispatch={dispatch} />
+      );
+    }
   }
-}
-
-function Field() {
   return <div className="field">{squares}</div>;
-}
+};
 
 export default Field;
--- src/components/FieldCell/FieldCell.js
+++ src/components/FieldCell/FieldCell.js
@@ -4,10 +4,58 @@ import BlackFigure from "./BlackFigure.png";
 
 import "./FieldCell.scss";
 
-const FieldCell = () => {
+const FieldCell = ({ data, dispatch }) => {
   const [color, setColor] = useState("white");
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    dispatch({ type: "SET_DROP_DEPTH", dropDepth: data.dropDepth + 1 });
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    dispatch({ type: "SET_DROP_DEPTH", dropDepth: data.dropDepth - 1 });
+    if (data.dropDepth > 0) return;
+    dispatch({ type: "SET_IN_DROP_ZONE", inDropZone: false });
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    e.dataTransfer.dropEffect = "copy";
+    dispatch({ type: "SET_IN_DROP_ZONE", inDropZone: true });
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    let files = [...e.dataTransfer.files];
+
+    if (files && files.length > 0) {
+      const existingFiles = data.fileList.map((f) => f.name);
+      files = files.filter((f) => !existingFiles.includes(f.name));
+
+      dispatch({ type: "ADD_FILE_TO_LIST", files });
+      e.dataTransfer.clearData();
+      dispatch({ type: "SET_DROP_DEPTH", dropDepth: 0 });
+      dispatch({ type: "SET_IN_DROP_ZONE", inDropZone: false });
+    }
+  };
+
   return (
-    <div className="cell">
+    <div
+      className="cell"
+      onDrop={(e) => handleDrop(e)}
+      onDragOver={(e) => handleDragOver(e)}
+      onDragEnter={(e) => handleDragEnter(e)}
+      onDragLeave={(e) => handleDragLeave(e)}
+    >
       {color ? (
         "black" ? (
           <img draggable src={BlackFigure} alt="black" />
